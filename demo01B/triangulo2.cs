d28c6dea4d531097b780cac87975eb832a48b6cb
brian Saavedra <brian.saavedra@gmail.com>
2024-08-20 21:07:17 -0500

    laborio1 2 comiit

--- demo01B/triangulo2.cs
+++ demo01B/triangulo2.cs
@@ -8,5 +8,19 @@ namespace demo01B
 {
     internal class triangulo2
     {
+        public string  x1 { get; set; }
+
+        public string y1 { get; set; }
+        public string x2 { get; set; }
+        public string y2 { get; set; }
+        public string x3 { get; set; }
+        public string y3 { get; set; }
+
+
     }
+
+    public int calcularPerimetro()
+        {
+        return A + B + C; 
+}
 }
